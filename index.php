0c43167e0f0e128d079149434525c3a15008b88d
Erick Peneluca <erickpeneluca@gmail.com>
2023-10-24 04:15:10 -0300

    testing-forms

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require('header.php')
+?>
+
+<form method="post" action="receiver.php">
+    <label >
+        Nome:
+        <br>
+        <input type="text" name="nome" id=""/>
+    </label>
+        <br>
+        <br>
+    </br>
+
+
+    <label >
+        Idade:
+        <br>
+        <input type="text" name="idade" id=""/>
+    </label>
+        <br>
+        <br>
+    </br>
+
+
+    <input type="submit" value="Enviar">
+</form>
